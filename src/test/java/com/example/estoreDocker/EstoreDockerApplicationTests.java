f13988860b0f7ce2c89c2201eb7d5bf34f2f83c8
Jiacs <74620286+Jiacs@users.noreply.github.com>
2024-01-22 02:23:06 +0800

    second commit

---
+++ src/test/java/com/example/estoreDocker/EstoreDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estoreDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoreDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
